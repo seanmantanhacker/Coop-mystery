fb780bfcfa43ad871f5a24503a5a3e605c53d308
sean pribadi <oyeng.bot@gmail.com>
2026-09-17 13:48:47 +0900

    asd

--- test-isolated-spawn.js
+++
@@ -1,8 +0,0 @@
-const { spawn } = require('child_process');
-const os = require('os');
-const env = { ...process.env, AGY_APP_DATA_DIR: os.tmpdir() + '/agy-isolated' };
-const aiProcess = spawn('agy.exe', ['-i', '--dangerously-skip-permissions'], { env });
-aiProcess.stdout.on('data', d => console.log('OUT:', d.toString()));
-aiProcess.stderr.on('data', d => console.log('ERR:', d.toString()));
-aiProcess.write = (data) => aiProcess.stdin.write(data);
-aiProcess.write('hello\n');
